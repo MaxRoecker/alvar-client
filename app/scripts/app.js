adf2f1c2c3b3cd37dcaa42798353ffdd0fa9770a
MaxRoecker <max.roecker@gmail.com>
2015-12-01 21:33:47 -0200

    Fixed bugs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -30,41 +30,78 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
   // See https://github.com/Polymer/polymer/issues/1381
   window.addEventListener('WebComponentsReady', function() {
+    var resourceViewList = document.querySelector('resource-view-list');
+    resourceViewList.refresh();
     // imports are loaded and elements have been registered
 
-    var login = document.querySelector('alvar-login');
-    var resourceView = document.querySelector('resource-view');
-    var resourceViewList = document.querySelector('resource-view-list');
-    var resourceCreateInterface = document.querySelector('resource-create-interface');
-    var drawerPanel = document.querySelector('#paperDrawerPanel');
-
-    var menuHandler =  function(event){
-      if (drawerPanel.narrow) {
-        drawerPanel.openDrawer();
-      }
-    };
-
-    login.addEventListener('authorized',function(){
-      login.close();
-    });
-
-    resourceViewList.refresh();
-    resourceViewList.addEventListener('menu',menuHandler);
-
-    resourceView.addEventListener('menu',menuHandler);
-
-    resourceCreateInterface.addEventListener('unauthorized',function(){
-      login.open();
-    });
-    
-    resourceCreateInterface.addEventListener('resource-created',function(){
-      page.redirect('/');
-      resourceViewList.refresh();
-    });
-
-    resourceCreateInterface.addEventListener('menu',menuHandler);
+    // var login = document.querySelector('alvar-login');
+    // var drawerPanel = document.querySelector('#paperDrawerPanel');
+    // var resourceView = document.querySelector('resource-view');
+    // var resourceViewList = document.querySelector('resource-view-list');
+    // var resourceCreateInterface = document.querySelector('resource-create-interface');
+    // var menu = document.querySelector('')
+    //
+    // login.addEventListener('authorized',function(){
+    //   login.close();
+    // });
+    //
+    // resourceViewList.refresh();
+    // resourceViewList.addEventListener('menu',menuHandler);
+    //
+    // resourceView.addEventListener('menu',menuHandler);
+    //
+    // resourceCreateInterface.addEventListener('unauthorized',function(){
+    //   login.open();
+    // });
+    //
+    // resourceCreateInterface.addEventListener('resource-created',function(){
+    //   page.redirect('/');
+    //   resourceViewList.refresh();
+    // });
+    //
+    // resourceCreateInterface.addEventListener('menu',menuHandler);
 
   });
+
+  //Open login modal
+  app.openLogin = function () {
+    var login = document.querySelector('alvar-login');
+    login.open();
+  };
+
+  //Close login modal
+  app.closeLogin = function (){
+    var login = document.querySelector('alvar-login');
+    login.close();
+  };
+
+  //Open menu if screen is narrow
+  app.openMenu = function () {
+    var drawerPanel = document.querySelector('#paperDrawerPanel');
+    if (drawerPanel.narrow) {
+      drawerPanel.openDrawer();
+    }
+  };
+
+  //Close menu if the screen is narrow
+  app.closeMenu = function(){
+    var drawerPanel = document.querySelector('#paperDrawerPanel');
+    if (drawerPanel.narrow) {
+      drawerPanel.closeDrawer();
+    }
+  }
+
+  //Redirect the user to home page
+  app.redirectToHome = function () {
+    var resourceViewList = document.querySelector('resource-view-list');
+    resourceViewList.refresh();
+    page.redirect('/');
+  };
+
+  //Redirect the user to previous page
+  app.redirectToPrevious = function () {
+    window.history.back();
+  };
 
   // Close drawer after menu item is selected if drawerPanel is narrow
   app.onMenuSelect = function() {
@@ -76,7 +113,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
   // Scroll page to top and expand header
   app.scrollPageToTop = function() {
-    document.getElementById('mainContainer').scrollTop = 0;
+     document.getElementById('mainContainer').scrollTop = 0;
   };
 
 })(document);
